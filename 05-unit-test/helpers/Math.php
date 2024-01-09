7fb24e8373f03cef22af44226c70aeb0d66d9f56
Josemi Xeon <worldcode.info@gmail.com>
2024-01-09 20:09:29 +0100

    Nuevos test en el curso de clean code

--- 05-unit-test/helpers/Math.php
+++ 05-unit-test/helpers/Math.php
@@ -1,1 +1,32 @@
 <?php
+namespace Helpers;
+require_once __DIR__ . "/../config/app.php";
+use DivisionByZeroError;
+
+class Math {
+	private float $firstValue, $secondValue;
+
+	public function __construct(float $firstValue, float $secondValue)
+	{
+		$this->firstValue = $firstValue;
+		$this->secondValue = $secondValue;
+	}
+	public function sum(): float {
+		return $this->firstValue + $this->secondValue;
+	}
+
+	public function subtraction(): float {
+		return $this->firstValue - $this->secondValue;
+	}
+
+	public function multiply(): float {
+		return $this->firstValue * $this->secondValue;
+	}
+
+	public function division(): float {
+		if($this->secondValue === 0.0){
+			throw new DivisionByZeroError("No puedes dividir entre cero");
+		}
+		return $this->firstValue / $this->secondValue;
+	}
+}
--- 05-unit-test/test/helpers/MathTest.php
+++ 05-unit-test/test/helpers/MathTest.php
@@ -1,9 +1,61 @@
 <?php
+require __DIR__.'/../../config/app.php';
+
+
+use Helpers\Math;
 
 use PHPUnit\Framework\TestCase;
 
 class MathTest extends TestCase {
-	public function testSumaDeDosNumeros() {
-		$this->assertEquals(4, 2 + 3);
+	public function testSumaDeDosNumerosEnteros() {
+		// AAA Pattern - Patrón de las tres A
+
+		// 1- Arrange = Organizar o inicializar
+		$math = new Math(40, 10);
+
+		// 2- Act = Actuar
+		$resultSum = $math->sum();
+		
+		// 3- Assert = Comprobar
+		$this->assertEquals(50, $resultSum);
+	}
+
+	public function testSumaDeDosNumerosFlotantes() {
+		// AAA Pattern - Patrón de las tres A
+
+		// 1- Arrange = Organizar o inicializar
+		$math = new Math(5.5, 8.8);
+
+		// 2- Act = Actuar
+		$resultSum = $math->sum();
+		
+		// 3- Assert = Comprobar
+		$this->assertEquals(14.3, $resultSum);
+	}
+
+	public function testSubtraction(){
+		$math = new Math(5, 8);
+		$result = $math->subtraction();
+		$this->assertEquals(-3, $result);
+	}
+
+	public function testMultiply(){
+		$math = new Math(5, 8);
+		$result = $math->multiply();
+		$this->assertEquals(40, $result);
+	}
+
+	public function testDivision(){
+		$math = new Math(5, 2);
+		$result = $math->division();
+		$this->assertEquals(2.5, $result);
+	}
+
+	public function testDivisionByZero(){
+		$math = new Math(5, 0);
+		$this->expectException(DivisionByZeroError::class);
+		$this->expectExceptionMessage(trad());
+		$result = $math->division();
+		
 	}
 }
